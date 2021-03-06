8edb404813094d30467d62d99f9f9d71f1f6a2b6
Shashank Biplav <Biplavshashank7@gmail.com>
2021-03-06 12:30:36 +0530

    [Model]Defined seller model

---
+++ models/seller.js
@@ -0,0 +1,42 @@
+import Sequelize from "sequelize";
+
+import sequelize from "../utilities/database.js";
+
+const Seller = sequelize.define("seller", {
+  id:{
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  password:{
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  phone: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  otp: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  isVerified: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  isAuthorized: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+export default Seller;
