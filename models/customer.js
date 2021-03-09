b559ba14af8e8a69b4bc8ff99a85caf6779dc74c
Shashank Biplav <Biplavshashank7@gmail.com>
2021-03-09 11:05:52 +0530

    [Model] Defined the customer schema

---
+++ models/customer.js
@@ -0,0 +1,50 @@
+import Sequelize from "sequelize";
+
+import sequelize from "../utilities/database.js";
+
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  phone: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  otp: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+  },
+  profileImageUrl:{
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  isVerified: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  isAuthorized: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: true,
+  },
+  refreshToken: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+});
+
+export default User;
