55bf1359ba35708f7aede2651aa6a587c4f653da
Shashank Biplav <Biplavshashank7@gmail.com>
2021-03-18 05:04:05 +0530

    [Endpoint] Admin can get all products paginated

--- controllers/admin/get-all-products.js
+++ controllers/admin/get-all-products.js
@@ -4,19 +4,16 @@ import Product from "../../models/product.js";
 //helpers
 import {validationErrorHandler} from "../../helpers/validation-error-handler.js";
 
-export const getAllSellerProducts = async (req, res, next) => {
+export const getAllProducts = async (req, res, next) => {
   validationErrorHandler(req, next);
   const {offset, limit} = req.body;
-  try {
+  try{
     const products = await Product.findAndCountAll({
-      where: {
-        userId: req.params.sellerId
-      },
-      offset: parseInt(offset),
+      offset:parseInt(offset),
       limit: parseInt(limit),
     });
     res.status(200).json({
-      message: 'Seller\'s products fetched successfully',
+      message: 'All products fetched successfully',
       products
     });
   } catch (err) {
--- routes/administrator-routes.js
+++ routes/administrator-routes.js
@@ -14,6 +14,7 @@ import {getAllBuyers} from "../controllers/admin/get-all-buyers.js";
 import {getAllSellers} from "../controllers/admin/get-all-sellers.js";
 import {getAllSellerProducts} from "../controllers/admin/get-seller-products.js";
 import {getAllAdminProducts} from "../controllers/admin/get-admin-products.js";
+import {getAllProducts} from "../controllers/admin/get-all-products.js";
 
 //middlewares
 import {isAdministrator} from "../middleware/is-administrator.js";
@@ -21,59 +22,65 @@ import {isAdministrator} from "../middleware/is-administrator.js";
 const router = express.Router();
 
 //GET ALL USERS
-router.post('/users', isAdministrator,[
+router.post('/users', isAdministrator, [
   body('offset').trim().isInt().withMessage("Stock must be an integer"),
   body('limit').trim().isInt().withMessage("Stock must be an integer"),
 ], getAllUsers);
 
+// GET ALL PRODUCTS
+router.post('/all-products', isAdministrator, [
+  body('offset').trim().isInt().withMessage("Stock must be an integer"),
+  body('limit').trim().isInt().withMessage("Stock must be an integer"),
+], getAllProducts);
+
 //GET ALL BUYERS
-router.post('/buyers', isAdministrator,[
+router.post('/buyers', isAdministrator, [
   body('offset').trim().isInt().withMessage("Stock must be an integer"),
   body('limit').trim().isInt().withMessage("Stock must be an integer"),
 ], getAllBuyers);
 
 //GET ALL SELLERS
-router.post('/sellers', isAdministrator,[
+router.post('/sellers', isAdministrator, [
   body('offset').trim().isInt().withMessage("Stock must be an integer"),
   body('limit').trim().isInt().withMessage("Stock must be an integer"),
 ], getAllSellers);
 
 //GET ALL SELLER'S PRODUCTS
-router.post('/seller-products/:sellerId', isAdministrator,[
+router.post('/seller-products/:sellerId', isAdministrator, [
   body('offset').trim().isInt().withMessage("Stock must be an integer"),
   body('limit').trim().isInt().withMessage("Stock must be an integer"),
 ], getAllSellerProducts);
 
 // GET ALL ADMIN PRODUCTS
-router.post('/my-products', isAdministrator,[
+router.post('/my-products', isAdministrator, [
   body('offset').trim().isInt().withMessage("Stock must be an integer"),
   body('limit').trim().isInt().withMessage("Stock must be an integer"),
 ], getAllAdminProducts);
 
 //UPDATE ADMIN PROFILE
-router.post('/update-profile', isAdministrator,[
+router.post('/update-profile', isAdministrator, [
   body('name').not().isEmpty().trim().escape().withMessage("Name is required"),
   body('email').isEmail().normalizeEmail().withMessage("Should be in a valid email format"),
-  body('password').trim().isLength({min:6}).withMessage("Password must be 6 characters long")
+  body('password').trim().isLength({min: 6}).withMessage("Password must be 6 characters long")
 ], changeAdministratorDetails);
 
 //CREATE NEW PRODUCT
-router.post('/add-product', isAdministrator,[
+router.post('/add-product', isAdministrator, [
   body('title').not().isEmpty().trim().escape().withMessage("Title is required"),
   body('price').isNumeric().not().isEmpty().withMessage("Should be in a Decimal format"),
   body('costPrice').isNumeric().not().isEmpty().withMessage("Should be in a Decimal format"),
   body('discount').isNumeric().not().isEmpty().withMessage("Should be in a Decimal format"),
-  body('description').not().isEmpty().trim().isLength({min:20}).escape().withMessage("Description is required"),
+  body('description').not().isEmpty().trim().isLength({min: 20}).escape().withMessage("Description is required"),
   body('stock').trim().isInt().withMessage("Stock must be an integer")
 ], createNewAdminProduct);
 
 //EDIT AN EXISTING PRODUCT
-router.put('/edit-product/:productId',isAdministrator,[
+router.put('/edit-product/:productId', isAdministrator, [
   body('title').not().isEmpty().trim().escape().withMessage("Title is required"),
   body('price').isNumeric().not().isEmpty().withMessage("Should be in a Decimal format"),
   body('costPrice').isNumeric().not().isEmpty().withMessage("Should be in a Decimal format"),
   body('discount').isNumeric().not().isEmpty().withMessage("Should be in a Decimal format"),
-  body('description').not().isEmpty().trim().isLength({min:20}).escape().withMessage("Description is required"),
+  body('description').not().isEmpty().trim().isLength({min: 20}).escape().withMessage("Description is required"),
   body('stock').trim().isInt().withMessage("Stock must be an integer"),
 ], editAdminProduct);
 
@@ -81,14 +88,14 @@ router.put('/edit-product/:productId',isAdministrator,[
 router.put('/edit-image/:productId', isAdministrator, updateFeaturedImageOfProduct);
 
 //EDIT BUYER, SELLER, AUTHORIZATION STATUS OF A USER
-router.patch('/edit-user/:userId', isAdministrator,[
+router.patch('/edit-user/:userId', isAdministrator, [
   body('isBuyer').not().isEmpty().isBoolean().withMessage('This is a required boolean'),
   body('isSeller').not().isEmpty().isBoolean().withMessage('This is a required boolean'),
   body('isAuthorized').not().isEmpty().isBoolean().withMessage('This is a required boolean'),
 ], toggleUserStatus);
 
 //EDIT PRODUCT PARAMETERS
-router.patch('/edit-product/:productId', isAdministrator,[
+router.patch('/edit-product/:productId', isAdministrator, [
   body('isActive').not().isEmpty().isBoolean().withMessage('This is a required boolean'),
   body('isTrending').not().isEmpty().isBoolean().withMessage('This is a required boolean'),
   body('isDealOfTheDay').not().isEmpty().isBoolean().withMessage('This is a required boolean'),
