b511b6c693bd6d639a651116fc876398ea71c0c9
Shashank Biplav <Biplavshashank7@gmail.com>
2021-03-18 01:31:27 +0530

    [Endpoint] Sellers can edit their existing products

--- routes/seller-routes.js
+++ routes/seller-routes.js
@@ -6,6 +6,7 @@ import {body} from 'express-validator';
 import {createNewSellerProduct} from "../controllers/seller/create-product.js";
 import {updateFeaturedSellerProductImage} from "../controllers/seller/update-featured-image.js";
 import {toggleSellerProductStatus} from "../controllers/seller/toggle-product-status.js";
+import {editSellerProduct} from "../controllers/seller/edit-product.js";
 
 //middleware
 import {isSeller} from "../middleware/is-seller.js";
@@ -22,8 +23,20 @@ router.post('/create-product', isSeller, [
   body('stock').trim().isInt().withMessage("Stock must be an integer")
 ], createNewSellerProduct);
 
+//EDIT AN EXISTING SELLER PRODUCT
+router.put('/edit-product/:productId', isSeller,[
+  body('title').not().isEmpty().trim().escape().withMessage("Title is required"),
+  body('price').isNumeric().not().isEmpty().withMessage("Should be in a Decimal format"),
+  body('costPrice').isNumeric().not().isEmpty().withMessage("Should be in a Decimal format"),
+  body('discount').isNumeric().not().isEmpty().withMessage("Should be in a Decimal format"),
+  body('description').not().isEmpty().trim().isLength({min:20}).escape().withMessage("Description is required"),
+  body('stock').trim().isInt().withMessage("Stock must be an integer"),
+], editSellerProduct);
+
+//EDIT FEATURED IMAGE OF AN EXISTING SELLER PRODUCT
 router.put('/update-image/:productId',isSeller, updateFeaturedSellerProductImage);
 
+//EDIT SELLER PRODUCT PARAMETERS
 router.put('/toggle-product/:productId', isSeller,[
   body('isActive').not().isEmpty().isBoolean().withMessage('This is a required boolean')
 ],toggleSellerProductStatus);
